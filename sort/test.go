28554c6f2e00d6b8401a6ec6cf42ae3a89e30a85
linlin <linlinblogs@sina.com>
2024-03-21 17:30:49 +0800

    快速排序的另一种写法

---
+++ sort/test.go
@@ -0,0 +1,39 @@
+package main
+// 快速排序
+import "fmt"
+// 基准
+// 小左，大右
+// 左边+ 右边的
+// 递归
+func sort(arr []int) []int{
+  if len(arr) <= 1 {
+    return arr
+  }
+  //length := len(arr)
+  result := []int{}
+  num := arr[0] // 4
+  left, right := []int{}, []int{}
+  mid := 0
+  for i := range arr {
+    if arr[i] < num {
+      right = append(right, arr[i])
+    } else if arr[i] == num {
+      mid = num
+    } else {
+      left = append(left, arr[i])
+    }
+  }
+
+  result = append(result, sort(left)...)
+  result = append(result, mid)
+  result = append(result, sort(right)...)
+
+  // left 4 2 1
+  return  result
+}
+
+func main() {
+  arr := []int{4, 2, 5, 1, 8}
+  result := sort(arr)
+  fmt.Println(result)
+}
